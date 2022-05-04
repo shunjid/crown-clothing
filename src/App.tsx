6f97819bc19da982791c91c7613875d55c416d8f
shunjid <shunjid.rahman@gmail.com>
2022-05-05 00:24:22 +0600

    Make iteration of categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,38 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* Image */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
+      {categories.map((category) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop now</p>
+          </div>
         </div>
-      </div>
-
-      <div className="category-container">
-        {/* Image */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* Image */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* Image */}
-        <div className="category-body-container">
-          <h2>Women</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* Image */}
-        <div className="category-body-container">
-          <h2>Men</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
