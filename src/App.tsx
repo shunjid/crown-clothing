d2ca068b377b8821b8756fc4777a38f0b1351b60
shunjid <shunjid.rahman@gmail.com>
2022-05-05 00:17:40 +0600

    Setup initial layout structure

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* Image */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* Image */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* Image */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* Image */}
+        <div className="category-body-container">
+          <h2>Women</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* Image */}
+        <div className="category-body-container">
+          <h2>Men</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
