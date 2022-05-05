dd4efa9a22063221660494dc52301b4bf98d6a31
shunjid <shunjid.rahman@gmail.com>
2022-05-06 02:38:44 +0600

    Add simple navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
+
 import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the Shop Page</h1>
+    </div>
+  )
+}
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+    </Route>
   </Routes>
 )
 
---
+++ src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,19 @@
+import { Outlet, Link } from 'react-router-dom'
+
+const Navigation = () => (
+  <>
+    <div className="navigation">
+      <Link className="logo-container" to="/">
+        <div>Logo</div>
+      </Link>
+      <div className="links-container">
+        <Link className="nav-link" to="/shop">
+          SHOP
+        </Link>
+      </div>
+    </div>
+    <Outlet />
+  </>
+)
+
+export default Navigation
