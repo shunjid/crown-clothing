d66f45194ad8a1e257c3364220a5338cbbdfed15
shunjid <shunjid.rahman@gmail.com>
2022-05-06 03:13:58 +0600

    Add styles to navigation

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,5 +1,6 @@
 import { Outlet, Link } from 'react-router-dom'
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
 
 const Navigation = () => (
   <>
@@ -7,7 +8,7 @@ const Navigation = () => (
       <Link className="logo-container" to="/">
         <CrownLogo className="logo" />
       </Link>
-      <div className="links-container">
+      <div className="nav-links-container">
         <Link className="nav-link" to="/shop">
           SHOP
         </Link>
