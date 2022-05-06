5b1f342214b8fc1409c1f7ae60b573258dc07ca0
shunjid <shunjid.rahman@gmail.com>
2022-05-07 01:15:58 +0600

    Change variable name to google provider

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -15,14 +15,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 })
 
 export const auth = getAuth()
-export const signInWithGooglePopUp = () => signInWithPopup(auth, provider)
-
+export const signInWithGooglePopUp = () => signInWithPopup(auth, googleProvider)
 export const db = getFirestore()
 
 export const createUserDocumentFromAuth = async (userAuth: {
