8b4996962c4c827d0da15837f02758ea5aad7626
shunjid <shunjid.rahman@gmail.com>
2022-05-06 22:51:55 +0600

    Add firebase config with auth setup

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopUp } from '../../utils/firebase/firebase.utils'
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopUp()
+    console.log(response)
+  }
+
   return (
     <div>
       <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign In with Google</button>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
