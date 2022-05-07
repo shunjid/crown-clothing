a3889b28342b807ce37dd0047e0d6f3c617f1a7b
shunjid <shunjid.rahman@gmail.com>
2022-05-07 14:37:10 +0600

    Add error handling for sign in

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -33,7 +33,15 @@ const SignInForm = () => {
       const response = await signInAuthUserWithEmailAndPassword(email, password)
       console.log(response)
       resetFormFields()
-    } catch (error) {}
+    } catch (error: any) {
+      if (error.code === 'auth/wrong-password') {
+        alert('Incorrect password or email')
+      } else if (error.code === 'auth/user-not-found') {
+        alert('User not found')
+      } else {
+        console.log('Encountered an error while signing in the user')
+      }
+    }
   }
 
   const logGoogleUser = async () => {
@@ -66,7 +74,7 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button clickHandler={logGoogleUser} dimension="google">
+          <Button type="button" clickHandler={logGoogleUser} dimension="google">
             Sign In with Google
           </Button>
         </div>
