a5ed84d95bc8bb5acadbff2c7781de7acfade730
shunjid <shunjid.rahman@gmail.com>
2022-05-22 23:07:27 +0600

    Initiate shop route

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Authentication from './routes/authentication/authentication.component'
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component'
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>I am the Shop Page</h1>
-    </div>
-  )
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => (
   <Routes>
